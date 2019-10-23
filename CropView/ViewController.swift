ab17149a9db8b723d7ad9210c3700666d984acd0
Michele Mola <michele.mola92@gmail.com>
2019-10-23 19:24:45 +0200

    Initial Commit

---
+++ CropView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CropView
+//
+//  Created by Michele Mola on 23/10/2019.
+//  Copyright © 2019 Michele Mola. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
